36fde63fc1b02772d822d180eb21638c2a84ffd1
AdanAnnaba <jubayeradan25@gmail.com>
2024-10-16 21:22:05 +0600

    added env port

--- index.js
+++ index.js
@@ -55,4 +55,4 @@ app.get('/download/:videoId', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000 || process.env.PORT, () => console.log('Server running on port 3000'));
